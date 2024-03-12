c64b0e02c50d7c990b8aef5c25a398a2dcbb2aaa
Tharun Atithya <tharunaadhi6@gmail.com>
2024-03-12 16:06:57 +0530

    last

--- q21.java
+++ q21.java
@@ -1,5 +1,13 @@
 public class q21 {
     public static void main(String[] args) {
-        
+        int cost_price = 1000;
+        int selling_price = 104;
+        int proit_loss = cost_price - selling_price;
+        if(proit_loss>=0){
+            System.out.println("The loss is "+proit_loss);
+        }
+        else{
+            System.out.println("The profit is "+(proit_loss - (proit_loss *2)));
+        }
     }
 }
